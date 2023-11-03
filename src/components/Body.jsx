2ab2030c1891679ea0d2bb57366f25522ef03dc5
Gordon D <gordon.draine97@yahoo.com>
2023-11-02 20:27:01 -0400

    adding body section

---
+++ src/components/Body.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <section className="section-4">
+      <h1 className="section-4-heading">Get started with PayPal.</h1>
+      <div className="section-4-content">
+        <div className="section-4-img-wrapper">
+          <img src="images/macbook.png" alt="macbook" />
+        </div>
+        <ul className="section-4-list">
+          <li className="section-4-list-item">
+            <span className="number">1</span>
+            <span>
+              Set up <a href="#">a business account</a>
+            </span>
+          </li>
+          <li className="section-4-list-item">
+            <span className="number">2</span>
+            <span>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              esse?
+            </span>
+          </li>
+          <li className="section-4-list-item">
+            <span className="number">3</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Body
