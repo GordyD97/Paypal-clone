f9ea77b965ca180186d16d16ce4160762d291126
Gordon D <gordon.draine97@yahoo.com>
2023-11-16 23:53:03 -0500

    adding more page routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header';
 import Body from './components/Body';
+import Footer from './components/Footer';
+import NavBar from './components/Navbar';
+import Testimonials from './components/Testimonials';
+
 import './App.css'
 
 function App() {
@@ -10,9 +14,11 @@ function App() {
 
   return (
     <div className='App'>
+      <NavBar />
       <Header />
       <Body />
-
+      <Testimonials />
+      <Footer />
     </div>
   )
 }
