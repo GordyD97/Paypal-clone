34613509ee025a795a687c74e5ac3029de63d1f7
Gordon D <gordon.draine97@yahoo.com>
2023-11-17 23:51:47 -0500

    adding longin form

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-const index = () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // form submisison.
+
+    console.log("Form submitted:", formData);
+    // data will be sent to server
+    // server not set up yet 
+  };
+
   return (
-    <div>index</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <br />
 
-export default index
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <br />
+
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <br />
+
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <br />
+
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUpForm;
